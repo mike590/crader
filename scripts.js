60dad23132ef04c91ce587b040547fcbe31e558d
Michael Gherard <mike@spotible.com>
2018-01-24 16:57:47 -0500

    wrapped await calls in async function

--- scripts.js
+++ scripts.js
@@ -3,8 +3,9 @@ var ccxt = require ('ccxt');
 // console.log (ccxt.exchanges);
 
 let binance  = new ccxt.binance ({ verbose: true });
-// console.log (binance.id,  await binance.loadMarkets());
-// console.log (binance.id,  await binance.fetchTicker('BTC/USD'));
-// await isn't working
-console.log (binance.id,  binance.loadMarkets());
-console.log (binance.id,  binance.fetchTicker('BTC/USD'));
+async function test() => {
+  x = await binance.loadMarkets();
+  // y = await binance.fetchTicker('BTC/USD');
+  debugger
+}
+setTimeout(test, 5000)
