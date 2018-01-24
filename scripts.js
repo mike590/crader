dfe9433c9c1a8d19552de19aa41e5e78eefeeaf9
mike590 <mike.gherard@gmail.com>
2018-01-23 23:43:37 -0500

    found bug with await

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 var ccxt = require ('ccxt');
 
-console.log (ccxt.exchanges);
+// console.log (ccxt.exchanges);
+
+let binance  = new ccxt.binance ({ verbose: true });
+// console.log (binance.id,  await binance.loadMarkets());
+// console.log (binance.id,  await binance.fetchTicker('BTC/USD'));
+// await isn't working
+console.log (binance.id,  binance.loadMarkets());
+console.log (binance.id,  binance.fetchTicker('BTC/USD'));
