4a5c54f1728bbdc02bcd37230865d54bdb5f7253
mike590 <mike.gherard@gmail.com>
2018-02-01 01:06:55 -0500

    begining mongo logic, figuring out exchange api

---
+++ db.js
@@ -0,0 +1,48 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'crader';
+
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
+
+const countDocs = function(db, callback) {
+	const collection = db.collection('documents');
+		collection.find({}).toArray(function(err, docs) {
+		assert.equal(err, null);
+		console.log("Found the following records");
+		console.log(docs.length);
+		callback(docs);
+  });
+}
+
+
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  countDocs(db, function() {
+    client.close();
+  });
+
+});
+
--- scripts.js
+++ scripts.js
@@ -1,11 +1,60 @@
-var ccxt = require ('ccxt');
+const ccxt = require ('ccxt');
 
 // console.log (ccxt.exchanges);
 
-let binance  = new ccxt.binance ({ verbose: true });
-async function test() => {
-  x = await binance.loadMarkets();
+const binance = new ccxt.binance({ verbose: false });
+
+(async () => {
+  // x = await binance.loadMarkets();
   // y = await binance.fetchTicker('BTC/USD');
-  debugger
+  //
+  // const trades = await binance.fetchTrades('ETH/BTC');
+  // const prices = trades.map((trade) => trade.price);
+  // const max = Math.max(...prices);
+  // const min = Math.min(...prices);
+  // const mean = prices.reduce((acc, cV) => acc + cV)/prices.length;
+  // const spread = max - min;
+  // console.log(min, max, spread, mean);
+  // console.log(spread / mean * 100);
+  const symbols = ['ETH/BTC', 'XRP/ETH', 'XLM/ETH', 'VEN/ETH'];
+  const dayInMillieconds = 1000 * 60 * 60 * 24;
+  const tenDaysAgo = Date.now() - 10 * dayInMillieconds;
+  console.log(tenDaysAgo);
+  let sleep = (ms) => new Promise (resolve => setTimeout (resolve, ms));
+	if (binance.has.fetchOHLCV) {
+   //  for (const symbol of symbols) {
+   //    await sleep (binance.rateLimit); // milliseconds
+   //    ohlcv = await binance.fetchOHLCV (symbol, '1m', tenDaysAgo); // one minute
+			// console.log(ohlcv[ohlcv.length-1]);
+   //  }
+    ohlcv = await binance.fetchOHLCV ('XRP/ETH', '1m', tenDaysAgo); // one minute
+    // for (const t of ohlcv) {
+    // 	console.log(new Date(t[0]));
+    // }
+    // const first = ohlcv[0];
+    // const last = ohlcv[ohlcv.length-1][0];
+    // console.log(new Date(first[0]));
+    // console.log(new Date(last[0]));
+    // console.log(ohlcv.length);
+  }
+
+})()
+
+class Record {
+	constructor(date, base, quote, price) {
+		this.data = date;
+		this.base = base;
+		this.quote = quote;
+		this.price = price;
+	}
+
+	get() {
+		return {
+			date: this.data,
+			base: this.base,
+			quote: this.quote,
+			price: this.price
+		}
+	}
+
 }
-setTimeout(test, 5000)
