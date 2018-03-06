44d57d232cce12ec3d99e567ed28156030be17a3
mike590 <mike.gherard@gmail.com>
2018-03-06 00:04:56 -0500

    implemented ema, needs additional testing

--- scripts.js
+++ scripts.js
@@ -12,24 +12,24 @@ const minuteInMilliseconds = 1000 * 60;
 const hourInMilliseconds = minuteInMilliseconds * 60;
 const dayInMilliseconds = hourInMilliseconds * 24;
 // 86400000
-// const intervals = {'1m': minuteInMilliseconds};
-const intervals = {
-                    '1m': minuteInMilliseconds,
-                    '3m': minuteInMilliseconds * 3,
-                    '5m': minuteInMilliseconds * 5,
-                    '15m': minuteInMilliseconds * 15,
-                    '30m': minuteInMilliseconds * 30,
-                    '1h': hourInMilliseconds,
-                    '2h': hourInMilliseconds * 2,
-                    '4h': hourInMilliseconds * 4,
-                    '6h': hourInMilliseconds * 6,
-                    '8h': hourInMilliseconds * 8,
-                    '12h': hourInMilliseconds * 12,
-                    '1d': dayInMilliseconds,
-                    '3d': dayInMilliseconds * 3,
-                    '1w': dayInMilliseconds * 7,
-                    '1M': dayInMilliseconds * 31
-                  };
+const intervals = {'1m': minuteInMilliseconds};
+// const intervals = {
+//                     '1m': minuteInMilliseconds,
+//                     '3m': minuteInMilliseconds * 3,
+//                     '5m': minuteInMilliseconds * 5,
+//                     '15m': minuteInMilliseconds * 15,
+//                     '30m': minuteInMilliseconds * 30,
+//                     '1h': hourInMilliseconds,
+//                     '2h': hourInMilliseconds * 2,
+//                     '4h': hourInMilliseconds * 4,
+//                     '6h': hourInMilliseconds * 6,
+//                     '8h': hourInMilliseconds * 8,
+//                     '12h': hourInMilliseconds * 12,
+//                     '1d': dayInMilliseconds,
+//                     '3d': dayInMilliseconds * 3,
+//                     '1w': dayInMilliseconds * 7,
+//                     '1M': dayInMilliseconds * 31
+//                   };
 
 async function asyncForEach(array, callback) {
   for (let index = 0; index < array.length; index++) {
@@ -63,19 +63,19 @@ MongoClient.connect(url, function(err, client) {
     let int;
     let oldestRec;
 
-    // Split into ttwo functions, fetch and save
+    // Split into two functions, fetch and save
     // easy to split, but figure out how to implement within asyncWhile
     async function saveRecords() {
       let ohlcv;
       if (oldestRec) {
        ohlcv = await binance.fetchOHLCV ('ETH/USDT', int, oldestRec + intervals['1m']);
       } else {
-       ohlcv = await binance.fetchOHLCV ('ETH/USDT', int, Date.now()-(intervals['1m']*100));
+       ohlcv = await binance.fetchOHLCV ('ETH/USDT', int, Date.now()-(intervals['1m']*20));
       }
       await asyncForEach(ohlcv, async (rec) => {
         let typical = (rec[2] + rec[3] + rec[4]) / 3;
         // Repeat for the rest of the indicators
-        let tempMa = await ma(rec, int);
+        let {ma: tempMa, ema: tempEma} = await setIndicators(rec, int);
         await db.collection('pair_record').insertOne({
           'base': 'ETH',
           'quote': 'USDT',
@@ -87,7 +87,8 @@ MongoClient.connect(url, function(err, client) {
           'close': rec[4],
           'typical': typical,
           'volume': rec[5],
-          'ma': tempMa
+          'ma': tempMa,
+          'ema': tempEma
         });
       });
       if (ohlcv.length === 500) {
@@ -99,7 +100,7 @@ MongoClient.connect(url, function(err, client) {
       }
     }
 
-    async function ma(rec, interval) {
+    async function setIndicators(rec, interval) {
       const span = 90;
       // find the length of the span in milliSeconds by keying in, then multiply it by the
       // span (-1 so as not include the record being passed in) to get a start time for the query,
@@ -107,13 +108,39 @@ MongoClient.connect(url, function(err, client) {
       const spanStart = rec[0] - (intervals[interval] * (span - 1) + (intervals[interval] * 0.2)) ;
       const typical = (rec[2] + rec[3] + rec[4]) / 3;
       let docs = await db.collection('pair_record').find({'time': {'$gt': spanStart}, 'interval': interval}).sort({'time': -1}).toArray();
+      return {
+        'ma': ma(docs, typical),
+        'ema': ema(docs[0], typical)
+      };
+    }
+
+    function ma(docs, typical) {
       let tempMa = {};
       [4, 9, 24, 44, 69, 89].forEach(endIndex => {
         let subArray = docs.slice(0, endIndex);
         let reduction = subArray.reduce((a, cV) => {return a + cV.typical}, 0)
-        tempMa[endIndex+1] = (reduction + typical) / (subArray.length + 1);
+        tempMa[endIndex + 1] = (reduction + typical) / (subArray.length + 1);
       });
       return tempMa;
+    }
+
+    function ema(previousDoc, typical) {
+      if (previousDoc) {
+        let tempEma = {};
+        [6, 12, 26, 40].forEach(period => {
+          let multiplier = 2 / (period + 1);
+          let previousEma = previousDoc['ema'][period];
+          tempEma[period] = (typical - previousEma) * multiplier + previousEma;
+        });
+        return tempEma;
+      } else {
+        return {
+          6: typical,
+          12: typical,
+          26: typical,
+          40: typical
+        };
+      }
     }
 
     await asyncForEach(Object.keys(intervals), async (tempInt) => {
